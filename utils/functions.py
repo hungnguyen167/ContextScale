5c9e99e113636467e7d092d2343e8a6d5ae4a584
Hung Nguyen <vhnh1607@gmail.com>
2024-01-25 21:22:40 +0100

    New pipeline to construct party positions (2nd update)

--- utils/functions.py
+++ utils/functions.py
@@ -58,13 +58,55 @@ def group_texts(dataset, label_cols, text_col, group_factor=5):
     return(results)   
 
 
-def lr_code(code):
-    if code in [104,201,203,305,401,402,407,414,505,601,603,605,606,410]:                                                                                        
+def lrn_code(code_long, code_short):
+    if code_short in [
+        407, ## protectionism - negative
+        603, ## traditional morality - positive
+        204, ## constitutionalism: negative
+        507, ## education limitation
+        410, ## economic growth: positive
+        110, ## european integration: negative
+        302, ## centralization: positive
+        601, ## national way of life: positive
+        608, ## multiculturalism: negative
+        505, ## welfare state limitation
+        104, ## military: positive
+        102, ## foreign special relationships: negative
+        109, ## internationalism: negative
+        414, ## economic orthodoxy: positive
+        402, ## incentives: positive
+        401, ## free enterprise: positive 
+
+    ] or code_long in [
+        70302, ## agriculture and farmers: negative
+        60501 ## law and order: positive
+    ]:                                                                                        
         return('right')
-    elif code in [103,105,106,107,202,403,404,406,412,413,504,506,701,501,416]:                                                                            
+    elif code_short in [
+        406, ## protectionism: positive
+        604, ## traditional morality: negative
+        203, ## constitutionalism: positive
+        506, ## education expansion
+        416, ## antigrowth economy: positive
+        501, ## environment protection
+        108, ## european integration: positive
+        301, ## decentralization: positive
+        602, ## national way of life: negative
+        607, ## multiculturalism: positive
+        504, ## welfare state expansion
+        105, ## military negative
+        101, ## foreign special relationships: positive
+        107, ## internationalism: positive
+        409, ## keynesian demand management: positive
+        403, ## market regulation: positive
+        404, ## economic planning: positive
+    ] or code_long in [
+        70301, ## agriculture and farmers: positive
+        60502, ## law and order: negative
+    ]:                                                                            
         return('left')
     else:
-        return('neutral')
+        return('neutral/unknown')
     
 
 def coalition_lr(code_long, code_short):
@@ -375,3 +417,74 @@ def lowe_lr(code):
         return('negative')
     else:
         return('unknown')
+
+def train_loop(dataloader, model, optimizer, scheduler, loss_fn_topic, loss_fn_lr, loss_fn_reconstruct, device):
+    print("")
+    print('Training...')
+
+    # Measure how long the training epoch takes.
+    t0 = time.time()
+
+    # Put the model into training mode. 
+    size = len(dataloader.dataset)
+    model.train()
+    train_loss = 0
+    # For each batch of training data...optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
+    for batch_num, batch in enumerate(dataloader):
+      batch = {k: v.to(device) for k,v in batch.items()}
+      optimizer.zero_grad()
+      y_topic = batch['topic'].long()
+      y_lr = batch['lr'].long()
+      logits_topic, logits_lr, logits_reconstruct, roberta_output = model(input_ids = batch['input_ids'], 
+                                      attention_mask = batch['attention_mask'])
+      loss_topic = loss_fn_topic(logits_topic, y_topic)
+      loss_lr = loss_fn_lr(logits_lr, y_lr)
+      loss_reconstruct = loss_fn_reconstruct(logits_reconstruct, roberta_output)
+      loss = 0.3*loss_topic + 0.5*loss_lr + 0.2*loss_reconstruct
+      # Backpropagation
+      loss.backward()
+      torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+      optimizer.step()
+      scheduler.step()
+      train_loss += loss.item()
+      # Report
+      if batch_num % 1000 == 0 and batch_num != 0:
+        elapsed = format_time(time.time() - t0)
+        current_loss = train_loss/batch_num
+        current = batch_num * len(batch['input_ids'])
+        print(f"loss: {current_loss:>7f}  [{current:>5d}/{size:>5d}]. Took {elapsed}")
+    
+
+  
+    # Measure how long this epoch took.
+    training_time = format_time(time.time() - t0)
+
+    print("")
+    print("  Training epoch took: {:}".format(training_time))
+def eval_loop(dataloader, model, loss_fn, loss_fn_lr):
+  size = len(dataloader.dataset)
+  num_batches = len(dataloader)
+  test_loss, correct_topic, correct_sent = 0.0, 0.0, 0.0
+  model.eval()
+  with torch.no_grad():  
+    for batch in dataloader:    
+        batch = {k: v.to(device) for k,v in batch.items()}
+        y_topic = batch['topic'].long()
+        y_lr = batch['lr'].long()
+        logits_topic, logits_lr = model(input_ids = batch['input_ids'], 
+                                        attention_mask = batch['attention_mask'],
+                                        token_type_ids=batch['token_type_ids'])
+        loss_topic = loss_fn(logits_topic, y_topic)
+        loss_lr = loss_fn_lr(logits_lr, y_lr)
+        loss = loss_topic + loss_lr
+        test_loss += loss
+        correct_topic += (logits_topic.argmax(1) == batch['topic']).type(torch.float).sum().item()
+        correct_sent += (logits_lr.argmax(1) == batch['lr']).type(torch.float).sum().item()
+
+  test_loss /= num_batches
+  correct_topic /= size
+  correct_sent /= size
+  correct = (correct_topic+correct_sent)/2
+  accuracy = correct*100
+  print(f"Test Error: \n Accuracy: {(accuracy):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+  return(accuracy)
